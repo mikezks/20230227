8c3acca0c0a0c86599c9872997568208487115a7
mikezks <23002661+mikezks@users.noreply.github.com>
2023-02-26 17:20:49 +0100

    feat: routing config

--- single-spa-angular-mfe/projects/mfe-flight/src/app/app-routing.module.ts
+++ single-spa-angular-mfe/projects/mfe-flight/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+      path: '**',
+      component: EmptyRouteComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+      RouterModule.forRoot(routes)
+  ],
+  providers: [
+      { provide: APP_BASE_HREF, useValue: '/mfe-flight' }
+  ],
+  exports: [
+      RouterModule
+  ]
 })
 export class AppRoutingModule { }
